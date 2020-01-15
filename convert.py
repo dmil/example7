1173aa59ba843e2486da4bc4464717d8bddf1504
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2020-01-15 14:19:22 -0500

    get visualization working with new data

--- convert.py
+++ convert.py
@@ -18,15 +18,16 @@ with open('output.csv', 'w') as f:
 
 	# 3. Loop through each row of `polls` 
 	for poll in polls:
-		raw_date = poll['enddate']
-		approve = poll['approve']
+		if poll['pollster'] == 'Gallup':
+			raw_date = poll['enddate']
+			approve = poll['approve']
 
-		date = datetime.datetime.strptime(raw_date, "%m/%d/%Y")
-		new_date = date.strftime("%-d-%b-%y")
-		# 4. and within that loop... convert the format of 
-		#    `enddate` from "1/22/2017" to "22-Jan-17"
-		
-		# 5. write a new row of data with the transformed date and value for "approve" 
-		writer.writerow([new_date, approve])
+			date = datetime.datetime.strptime(raw_date, "%m/%d/%Y")
+			new_date = date.strftime("%-d-%b-%y")
+			# 4. and within that loop... convert the format of 
+			#    `enddate` from "1/22/2017" to "22-Jan-17"
+			
+			# 5. write a new row of data with the transformed date and value for "approve" 
+			writer.writerow([new_date, approve])
 
-# by Dhrumil and Sultan
+	# by Dhrumil and Sultan
